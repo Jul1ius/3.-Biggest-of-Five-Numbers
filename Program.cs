dac3ed64af7bbb56a2a217c6affbcf3f319363fb
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-29 16:29:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                numbers[i] = number;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                i--;
+            }
+        }
+
+        int biggest = numbers[0];
+
+        for (int i = 1; i < 5; i++)
+        {
+            if (numbers[i] > biggest)
+            {
+                biggest = numbers[i];
+            }
+        }
+
+        Console.WriteLine(biggest);
+    }
+}
